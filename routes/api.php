1ea50f432cddb3f18470ea517856e004d4674a89
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-11-16 22:14:55 +0200

    Caches the document index routes
    changes:
    - adds a new package laravel-responsecache
    - adds a new middleware alias for the new package
    - caches DocumentController@index for 300 seconds

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('documents', DocumentController::class);
+// Route::apiResource('documents', DocumentController::class);
+
+Route::get('documents', [DocumentController::class, 'index'])->middleware('cacheResponse:300');
+Route::get('documents/{document}', [DocumentController::class, 'show']);
+
+Route::post('documents', [DocumentController::class, 'store']);
+
+Route::patch('documents/{document}', [DocumentController::class, 'update']);
+
+Route::delete('documents/{document}', [DocumentController::class, 'destroy']);
